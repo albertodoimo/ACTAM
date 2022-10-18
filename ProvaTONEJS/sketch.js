24727915b15db2d750e1d2c39203bc91a00775af
riciacc <riccardo.iaccarino@mail.polimi.it>
2022-10-18 18:32:45 +0200

    Loop sbagliato

--- ProvaTONEJS/sketch.js
+++ ProvaTONEJS/sketch.js
@@ -1,16 +1,22 @@
 console.clear();
 
-const synth = new Tone.Synth();
-synth.toDestination();
+const synthFM = new Tone.AMSynth();
+synthFM.toDestination();
+
+const now = Tone.now()
+
+
 
 function playSound(){
-    synth.triggerAttackRelease('C4', '4n');
+    const loop = new Tone.Loop(time => {
+        synthFM.triggerAttackRelease("G4", "8n", time)
+        synthFM.triggerAttackRelease("B4", "8n", time + 0.5)
+        synthFM.triggerAttackRelease("E5", "8n", time + 1)
+        synthFM.triggerAttackRelease("B4", "8n", time + 1.5)
+    }, "n").start(0);
+    Tone.Transport.start()
 }
  
-
-
-
-
 
 
 
