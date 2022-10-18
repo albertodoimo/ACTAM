e502b750bd1b92bc65e48142a15e7d06090ede07
riciacc <riccardo.iaccarino@mail.polimi.it>
2022-10-18 17:47:15 +0200

    Butt

--- ProvaTONEJS/sketch.js
+++ ProvaTONEJS/sketch.js
@@ -3,4 +3,29 @@ console.clear();
 const synth = new Tone.Synth();
 synth.toDestination();
 
-synth.triggerAttackRelease('C4', '4n');
+function playSound(){
+    synth.triggerAttackRelease('C4', '4n');
+}
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
