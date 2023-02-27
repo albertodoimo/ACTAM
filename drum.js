b04815b4599b800f56eff1ac677e5d1420ea61eb
albertodoimo <101573928+albertodoimo@users.noreply.github.com>
2023-02-27 01:26:39 +0100

    added param on drum page

--- drum.js
+++ drum.js
@@ -64,7 +64,6 @@ let chromas = [
   "B",
 ];
 let chromas2 = ["C","Db","D","Eb","E","F","Gb","G","Ab","A","Bb","B"];
-let chromas2 = ["C","Db","D","Eb","E","F","Gb","G","Ab","A","Bb","B"];
 
 let myScale = [];
 let majProf = [0, 2, 4, 5, 7, 9, 11];
