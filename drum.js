4e49d95b7be369639d1b5ded03e37a22484e8708
albertodoimo <101573928+albertodoimo@users.noreply.github.com>
2023-03-17 14:10:21 +0100

    2d 3d ancora storti

--- drum.js
+++ drum.js
@@ -999,6 +999,10 @@ tour.addStep({
   },
   classes: '',
   buttons: [
+    {
+      text: 'NEXT',
+      action:tour.next
+    },
       {
         text: 'EXIT',
         action: tour.complete
@@ -1007,6 +1011,23 @@ tour.addStep({
   ]
 });
 
+// step #8
+tour.addStep({
+  id: '3d',
+  text: 'Move in the 3D space using the mouse ',
+  attachTo: {
+      element: '',
+      on: ''
+  },
+  classes: '',
+  buttons: [
+      {
+        text: 'EXIT',
+        action: tour.complete
+      },
+
+  ]
+});
 console.log(guideflag2);
 
 if(guideflag2==1){
